8bf1b1e91ef95750025784c43c92007b8cd95c99
mwq2020 <mwq2020@163.com>
2017-02-09 15:45:00 +0800

    fix

--- Application/Statistics/Model/Pagination.php
+++ Application/Statistics/Model/Pagination.php
@@ -73,7 +73,7 @@ class Pagination
      * @param array $listRows  每页显示记录数
      * @param array $parameter  分页跳转的参数
      */
-    public function __construct($totalRows, $listRows = 20, $parameter = array())
+    public function __construct($totalRows, $listRows = 10, $parameter = array())
     {
         //$this->p = isset($_GET['p']) ? $_GET['p'] : 1; //设置分页参数名称
         /* 基础设置 */
